2bf173e8bcc85fbd2754d819b4b0746928e09712
Theo Rondon <theoprondon@hotmail.com>
2023-10-23 23:30:28 -0300

    Alteracao de erro no código do botao em python

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run() 
-
 @app.route('/botao', methods=['POST'])
 def botao():
     return "Você clicou no botao!"
+
+if __name__ == '__main__':
+    app.run(debug=True) 
