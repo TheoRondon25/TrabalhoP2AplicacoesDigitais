aa374e3553558caaa65f712a07144a57a05ee9b3
Theo Rondon <theoprondon@hotmail.com>
2023-10-25 21:22:12 -0300

    criando código da tela de login e tela de cadastro

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def login():
+    return render_template('login.html')
 
-@app.route('/botao', methods=['POST'])
-def botao():
-    return "Você clicou no botao!"
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
 
+@app.route('/telaInicial', methods=['POST'])
+def telaInicial():
+    return "Você está na tela inicial!"  
+
+# Colocar o site no ar 
 if __name__ == '__main__':
     app.run(debug=True) 
